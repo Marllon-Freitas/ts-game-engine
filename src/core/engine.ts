ef4cad5eb75457dbb67b42e08a5beda9c36781e0
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-08-05 19:16:16 -0300

    feat: add translation to the sprite

--- src/core/engine.ts
+++ src/core/engine.ts
@@ -1,11 +1,13 @@
-import { AttributeInfo, GLBuffer } from './gl/glBuffer'
 import { Shader } from './gl/shader'
 import { gl, WebGLUtilities } from './gl/webgl'
+import { Sprite } from './graphics/sprite'
+import { Matrix4x4 } from './math/matrix4x4'
 
 export class Engine {
   private m_canvas!: HTMLCanvasElement
   private m_shader!: Shader
-  private m_buffer!: GLBuffer
+  private m_sprite!: Sprite
+  private m_projectionMatrix!: Matrix4x4
 
   public constructor() {}
 
@@ -16,7 +18,13 @@ export class Engine {
 
     this.loadShader()
     this.m_shader.useShader()
-    this.createBuffer()
+
+    this.updateProjectionMatrix()
+
+    this.m_sprite = new Sprite('test')
+    this.m_sprite.load()
+    this.m_sprite.position.x = 100
+    this.m_sprite.position.y = 100
 
     this.loop()
   }
@@ -29,6 +37,23 @@ export class Engine {
     this.m_canvas.width = window.innerWidth
     this.m_canvas.height = window.innerHeight
     gl.viewport(0, 0, this.m_canvas.width, this.m_canvas.height)
+
+    this.updateProjectionMatrix()
+  }
+
+  private updateProjectionMatrix(): void {
+    const aspectRatio = this.m_canvas.width / this.m_canvas.height
+    const orthoHeight = this.m_canvas.height
+    const orthoWidth = orthoHeight * aspectRatio
+
+    this.m_projectionMatrix = Matrix4x4.orthographic(
+      0,
+      orthoWidth,
+      0,
+      orthoHeight,
+      -1.0,
+      100.0
+    )
   }
 
   private loop(): void {
@@ -36,41 +61,35 @@ export class Engine {
 
     // set uniforms
     const colorLocation = this.m_shader.getUniformLocation('u_color')
-    gl.uniform4f(colorLocation, 1.0, 0.0, 0.0, 1.0)
+    gl.uniform4f(colorLocation, 1.0, 0.5, 0.0, 1.0)
 
-    this.m_buffer.bind()
-    this.m_buffer.draw()
+    const projectionLocation = this.m_shader.getUniformLocation('u_projection')
+    gl.uniformMatrix4fv(
+      projectionLocation,
+      false,
+      new Float32Array(this.m_projectionMatrix.data)
+    )
+
+    const modelLocation = this.m_shader.getUniformLocation('u_model')
+    gl.uniformMatrix4fv(
+      modelLocation,
+      false,
+      new Float32Array(Matrix4x4.translation(this.m_sprite.position).data)
+    )
+
+    this.m_sprite.draw()
 
     requestAnimationFrame(() => this.loop())
-  }
-
-  private createBuffer(): void {
-    this.m_buffer = new GLBuffer(3)
-    if (!this.m_buffer) {
-      throw new Error('[Engine] Error creating buffer')
-    }
-
-    const positionAttribute = new AttributeInfo()
-    positionAttribute.location =
-      this.m_shader.getAttributeLocation('a_position')
-    positionAttribute.offset = 0
-    positionAttribute.size = 3
-    this.m_buffer.addAttributeLocation(positionAttribute)
-
-    const vertices = [
-      // X,   Y,  Z
-      0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.5, 0.5, 0.0
-    ]
-    this.m_buffer.addData(vertices)
-    this.m_buffer.upload()
-    this.m_buffer.unbind()
   }
 
   private loadShader(): void {
     const vertexSource = `
       attribute vec3 a_position;
+      uniform mat4 u_projection;
+      uniform mat4 u_model;
+
       void main() {
-        gl_Position = vec4(a_position, 1.0);
+        gl_Position = u_projection * u_model * vec4(a_position, 1.0);
       }
     `
     const fragmentSource = `
