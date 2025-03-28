575b1437e30822b96cc3c4330cd2fb448fd67368
Marllon Freitas <marllonfreitas64@gmail.com>
2025-03-28 13:35:40 -0300

    feat: add base game loop

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,29 @@
 
+export class Engine {
+  constructor() {
+    console.log('Engine initialized');
+  }
+
+  private m_count: number = 0;
+
+  // private methods
+  private loop(): void {
+    console.log('Engine loop running');
+    this.m_count++;
+    document.body.innerHTML = `Hello World! ${this.m_count}`;
+    requestAnimationFrame(this.loop.bind(this));
+  }
+
+  // public methods
+  public start(): void {
+    console.log('Engine started');
+    this.loop();
+  }
+}
+
 window.onload = () => {
   document.body.innerHTML = 'Hello World!'
+  const engine = new Engine();
+  // engine.start();
+  console.log('Engine instance created:', engine);
 }
