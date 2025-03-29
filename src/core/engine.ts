a3bc8305f6e8b836a9a0bfa184e9cbae685c807c
Marllon Freitas <marllonfreitas64@gmail.com>
2025-03-29 01:00:29 -0300

    feat: implements the new webGL buffer class and the new uniform info from the shader

--- src/core/engine.ts
+++ src/core/engine.ts
@@ -1,5 +1,6 @@
 import { Shader } from './webGL/shader';
 import { WegGLUtilities } from './webGL/webGL';
+import { AttributeInfo, WGLBuffer } from './webGL/wGLBuffer';
 
 /**
  * The main engine class for the game.
@@ -8,27 +9,16 @@ export class Engine {
   // private methods and attributes:
   private m_canvas!: HTMLCanvasElement;
   private m_shader!: Shader;
-  private m_buffer!: WebGLBuffer;
+  private m_buffer!: WGLBuffer;
 
   private loop(): void {
     WegGLUtilities.gl.clear(WegGLUtilities.gl.COLOR_BUFFER_BIT);
 
-    WegGLUtilities.gl.bindBuffer(WegGLUtilities.gl.ARRAY_BUFFER, this.m_buffer);
+    let colorLocation = this.m_shader.getUniformLocation('u_color');
+    WegGLUtilities.gl.uniform4f(colorLocation, 1.0, 1.0, 0.0, 1.0);
 
-    if (!this.m_shader) throw new Error('Shader not loaded.');
-    let positionLocation = this.m_shader.getAttributeLocation('a_position');
-
-    WegGLUtilities.gl.vertexAttribPointer(
-      positionLocation,
-      3,
-      WegGLUtilities.gl.FLOAT,
-      false,
-      0,
-      0
-    );
-    WegGLUtilities.gl.enableVertexAttribArray(positionLocation);
-
-    WegGLUtilities.gl.drawArrays(WegGLUtilities.gl.TRIANGLES, 0, 3);
+    this.m_buffer.bind();
+    this.m_buffer.draw();
 
     requestAnimationFrame(this.loop.bind(this));
   }
@@ -42,32 +32,32 @@ export class Engine {
     `;
     let fragmentShaderSource = `
       precision mediump float;
+      uniform vec4 u_color;
       void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+        gl_FragColor = u_color;
       }
     `;
     this.m_shader = new Shader('basicShader', vertexShaderSource, fragmentShaderSource);
   }
 
   private createBuffer(): void {
-    this.m_buffer = WegGLUtilities.gl.createBuffer();
+    this.m_buffer = new WGLBuffer(3);
     if (!this.m_buffer) throw new Error('Unable to create buffer.');
 
-    WegGLUtilities.gl.bindBuffer(WegGLUtilities.gl.ARRAY_BUFFER, this.m_buffer);
-    const vertices = new Float32Array([
+    let positionAttribute = new AttributeInfo();
+    positionAttribute.location = this.m_shader.getAttributeLocation('a_position');
+    positionAttribute.offset = 0;
+    positionAttribute.size = 3;
+    this.m_buffer.setAttributeLocation(positionAttribute);
+
+    const vertices = [
       // x, y, z
       0, 0, 0, 0, 0.5, 0, 0.5, 0.5, 0
-    ]);
-    WegGLUtilities.gl.bufferData(
-      WegGLUtilities.gl.ARRAY_BUFFER,
-      vertices,
-      WegGLUtilities.gl.STATIC_DRAW
-    );
+    ];
 
-    let positionLocation = this.m_shader.getAttributeLocation('a_position');
-
-    WegGLUtilities.gl.bindBuffer(WegGLUtilities.gl.ARRAY_BUFFER, null);
-    WegGLUtilities.gl.disableVertexAttribArray(positionLocation);
+    this.m_buffer.pushBackData(vertices);
+    this.m_buffer.uploadData();
+    this.m_buffer.unbind();
   }
 
   // public methods and attributes:
