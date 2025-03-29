2883a20faefcafc04cd6432aa42b0d67a4353e36
Marllon Freitas <marllonfreitas64@gmail.com>
2025-03-29 14:41:53 -0300

    feat: add new constants info

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,11 @@
 export const CANVAS_ID = 'gameCanvas';
+
+// messages
+export enum MessagePriority {
+  NORMAL,
+  HIGH
+}
+
+// assets
+export const MESSAGE_ASSET_LOADER_ASSET_LOADED = 'MESSAGE_ASSET_LOADER_ASSET_LOADED:: ';
+export const SUPPORTED_FILE_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif'];
