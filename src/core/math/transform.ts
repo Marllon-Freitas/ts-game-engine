f94b52adb7b98322a016481148e0f976ef4cb513
Marllon Freitas <marllonfreitas64@gmail.com>
2025-03-30 13:53:36 -0300

    feat: add Transform class

---
+++ src/core/math/transform.ts
@@ -0,0 +1,23 @@
+import { Matrix4x4 } from './matrix4x4';
+import { Vector3 } from './vector3';
+
+export class Transform {
+  public position: Vector3 = Vector3.zero;
+  public rotation: Vector3 = Vector3.zero;
+  public scale: Vector3 = Vector3.one;
+
+  public copyFrom(transform: Transform): void {
+    this.position.copyFrom(transform.position);
+    this.rotation.copyFrom(transform.rotation);
+    this.scale.copyFrom(transform.scale);
+  }
+
+  public getTransformMatrix(): Matrix4x4 {
+    let translationMatrix: Matrix4x4 = Matrix4x4.translation(this.position);
+    let rotationMatrix: Matrix4x4 = Matrix4x4.rotationZ(this.rotation.z);
+    let scaleMatrix: Matrix4x4 = Matrix4x4.scale(this.scale);
+
+    // multiply matrices in the order: translation -> rotation -> scale
+    return Matrix4x4.multiply(Matrix4x4.multiply(translationMatrix, rotationMatrix), scaleMatrix);
+  }
+}
